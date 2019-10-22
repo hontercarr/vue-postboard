ca82bedec859d5bc8acedf4fbe85974f45294d46
Hunter Carr <hontercarr@gmail.com>
2019-10-21 19:55:52 -0500

    Version 0.0.1 - Vue Postboard

---
+++ src/views/About.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="about">
+        <h1>About</h1>
+        <p>
+            This is the Todo List app version 0.0.1.
+            <br />It is part of the Vue crash course on YouTube by Traversy Media.
+        </p>
+    </div>
+</template>
